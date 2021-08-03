bee54d762bb1d4701062d06d1a4eff078960732a
yurifardel <yrnascimento@outlook.com>
2021-08-03 15:20:48 -0300

    webpack

---
+++ public/assets/js/bundle.js
@@ -0,0 +1,9 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+console.log("ola");
+/******/ })()
+;
+//# sourceMappingURL=bundle.js.map
